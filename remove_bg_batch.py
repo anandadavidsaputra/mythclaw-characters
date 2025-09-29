8874c3405ed7da4e0a3424f594f94164ebad8f25
Ananda David Saputra <paijahkucing@gmail.com>
2025-09-29 19:16:14 +0700

    remove bg

--- remove_bg_batch.py
+++ remove_bg_batch.py
@@ -1,27 +1,28 @@
 from PIL import Image
 import os
 
-# folder khusus SSR
-ssr_folder = "C:\\Providd\\Portofolio\\mythclaw_characters\\ssr"
+# folder utama semua tier
+base_folder = r"C:\Providd\Portofolio\mythclaw_characters"
 
-for file in os.listdir(ssr_folder):
-    if file.lower().endswith(".png"):
-        path = os.path.join(ssr_folder, file)
-        print(f"Memproses {path} ...")
-        
-        img = Image.open(path).convert("RGBA")
-        datas = img.getdata()
+for root, dirs, files in os.walk(base_folder):
+    for file in files:
+        if file.lower().endswith(".png"):
+            path = os.path.join(root, file)
+            print(f"Memproses {path} ...")
+            
+            img = Image.open(path).convert("RGBA")
+            datas = img.getdata()
 
-        newData = []
-        for item in datas:
-            # jika pixel hampir putih -> buat transparan
-            if item[0] > 240 and item[1] > 240 and item[2] > 240:
-                newData.append((255, 255, 255, 0))
-            else:
-                newData.append(item)
+            newData = []
+            for item in datas:
+                # jika pixel hampir putih -> buat transparan
+                if item[0] > 240 and item[1] > 240 and item[2] > 240:
+                    newData.append((255, 255, 255, 0))
+                else:
+                    newData.append(item)
 
-        img.putdata(newData)
-        img.save(path)
-        print(f"{file} → selesai, background putih dihapus")
+            img.putdata(newData)
+            img.save(path)
+            print(f"{file} → selesai, background putih dihapus")
 
-print("Selesai semua gambar SSR!")
+print("Selesai semua gambar di semua folder!")
